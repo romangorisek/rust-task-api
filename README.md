1ede98af95734107e3d1076c1c840d6479317087
Roman Gorisek <roman.gorisek2@gmail.com>
2023-03-27 23:12:14 +0200

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+rest api example using rust and actix
+
+following https://www.youtube.com/watch?v=L8tWKqSMKUI
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod model;
+mod repository;
+
+use api::task::{complete_task, create_task, get_task, start_task};
+
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+use repository::ddb::DDBRepository;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    let config = aws_config::load_from_env().await;
+    let ddb_repo: DDBRepository = DDBRepository::init(String::from("task"), config.clone());
+    let ddb_data = Data::new(ddb_repo);
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+            .wrap(logger)
+            .app_data(ddb_data)
+            .service(get_task)
+            .service(create_task)
+            .service(start_task)
+            .service(complete_task)
+    })
+    .bind(("127.0.0.1", 80))?
+    .run()
+    .await
 }
